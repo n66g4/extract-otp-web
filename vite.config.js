d8f9f52749bfef41c58bcacf8e2476a429af7df1
Matthew Carroll <matthew.carroll@stand.earth>
2025-07-29 17:14:30 -0700

    https dev support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 /**
  * @see https://vitejs.dev/config/
  */
 export default defineConfig({
-  base: "/extract-otp-web/",
+  base: '/extract-otp-web/',
+  plugins: [basicSsl()],
+  server: {
+    https: true,
+  },
 });
